6bab844de7906e6602360a27944ec9b8f1fe444a
George Oster <georgeoster@macbook-air.myfiosgateway.com>
2023-04-30 16:09:45 -0400

    login and logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddPlaceComponent } from './components/add-place/add-place.component';
 import { ViewPlacesComponent } from './components/view-places/view-places.component';
+import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'addPlace', component: AddPlaceComponent },
   { path: 'viewPlaces', component: ViewPlacesComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: 'addPlace', pathMatch: 'full' }
 ];
 
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private loggedInSubject = new BehaviorSubject(false);
+  loggedIn = this.loggedInSubject.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  login(user:string, pass:string) {
+    this.http.get('http://localHost:3000/api/v1/login/'+user+'/'+pass).subscribe((response:any) => {
+      this.loggedInSubject.next(response.login);
+    });
+  }
+
+  logout(){
+    this.loggedInSubject.next(false);
+  }
+}
