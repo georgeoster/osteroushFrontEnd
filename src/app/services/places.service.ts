113e3e4336a60023db16ea0eb163aa8a93ba3f0d
George Oster <georgeoster@macbook-air.myfiosgateway.com>
2023-05-13 09:46:53 -0400

    switched api calls to server location

--- src/app/services/places.service.ts
+++ src/app/services/places.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-// import { Place } from '../types';
 import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
@@ -21,7 +20,7 @@ export class PlacesService {
 
   addPlace(place:FormData) {
     const year = place.get('year');
-    this.http.post('http://osteroush-env.eba-puhnr5j3.us-east-2.elasticbeanstalk.com/api/v1/place', place).subscribe((response:any) => {
+    this.http.post('https://osteroush.com/BackEnd/api/v1/place', place).subscribe((response:any) => {
       if (response?.success) {
         this.yearToShowSubject.next(year);
         this.placeAddedSubject.next(true);
@@ -30,19 +29,19 @@ export class PlacesService {
   }
 
   updatePlace(place:FormData) {
-    this.http.patch('http://osteroush-env.eba-puhnr5j3.us-east-2.elasticbeanstalk.com/api/v1/place', place).subscribe((response) => {
+    this.http.patch('https://osteroush.com/BackEnd/api/v1/place', place).subscribe((response) => {
       console.log(response);
     });
   }
 
   getPlaces(year:string) {
-    this.http.get('http://osteroush-env.eba-puhnr5j3.us-east-2.elasticbeanstalk.com/api/v1/places/'+year).subscribe((response) => {
+    this.http.get('https://osteroush.com/BackEnd/api/v1/places/'+year).subscribe((response) => {
       this.placesSubject.next(response);
     });
   }
 
   deletePlace(name:string, date:string) {
-    this.http.delete('http://osteroush-env.eba-puhnr5j3.us-east-2.elasticbeanstalk.com/api/v1/place/'+name+'/'+date).subscribe((response) => {
+    this.http.delete('https://osteroush.com/BackEnd/api/v1/place/'+name+'/'+date).subscribe((response) => {
       console.log(response);
     });
   }
