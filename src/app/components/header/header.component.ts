82ecfb4ae740896276bfcfb3bd926d28c4715e04
osteroush <36748185+osteroush@users.noreply.github.com>
2024-07-09 19:23:52 -0400

    added years to add and header (#13)
    Co-authored-by: George Oster <georgeoster@georges-air.myfiosgateway.com>

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,6 +11,9 @@ export class HeaderComponent {
 
   brand:string = 'OSTEROUSH';
   years=[
+    {label: '2026', value: '2026'},
+    {label: '2025', value: '2025'},
+    {label: '2024', value: '2024'},
     {label: '2023', value: '2023'},
     {label: '2022', value: '2022'},
     {label: '2021', value: '2021'},
