b2bd747612846fbe6682ce8864e18b1341b61409
georgeoster <georgeostercodes@gmail.com>
2025-04-26 17:19:50 -0400

    redesign: added page load and route change animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('routeAnimations', [
+      transition('* <=> *', [
+        style({ opacity: 0, transform: 'translateY(10px)' }),
+        animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' })),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'osteroush';
+  prepareRoute(outlet: any) {
+    return outlet.activatedRouteData?.['animation'];
+  }
 }
